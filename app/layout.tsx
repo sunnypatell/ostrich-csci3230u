dd2cff7e6ea756234fd5f945ad50d9165a325a44
sunnypatell <sunnypatel124555@gmail.com>
2025-04-01 22:40:18 -0400

    chore(metatags): title, desc, libgen

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'OSTRICH',
+  description: 'Open Source Tracking and Recon Intelligence for Cyber Hunting',
+  generator: 'www.sunnypatel.net',
 }
 
 export default function RootLayout({
